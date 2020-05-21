22d5194dbb7655c0796cd2d10b62b6e5e5d37478
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-05-21 14:54:09 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const TOKEN = ''; //token bot
+const PREFIX = '_';
+
+const fs = require('fs');
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+})
+
+client.on('message', async msg => {
+  if (msg.author.bot) return;
+  
+  if (msg.content.toLowerCase().startsWith(PREFIX)) {
+	const args = msg.content.slice(PREFIX.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (command == null) return;
+	try {
+		command.execute(msg, args, client, db);
+	} catch (error) {
+		return msg.reply(`Xảy ra lỗi khi thực hiện lệnh này:\n\`${error.message}\``);
+	}
+   }
+})
+
+client.login(TOKEN)
