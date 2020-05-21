0b740d12eceef82d8a518802b7f2aebbb7115439
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-05-21 15:21:00 +0700

    Create pause.js

---
+++ commands/pause.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'pause',
+	async execute(msg, args, client) {
+		if(!msg.member.voice.channel) return msg.reply(`Có ở trong kênh nhạc đâu!`);
+		client.player.pause(msg.guild.id).then(song => {
+			return msg.channel.send({embed: {title: `Tạm dùng phát nhạc:`, description: `${song.title}`, thumbnail: {url: song.thumbnail} }});
+		}).catch(err => {
+			return msg.reply(`Lỗi: \`${err}\``);
+		});
+	},
+};
